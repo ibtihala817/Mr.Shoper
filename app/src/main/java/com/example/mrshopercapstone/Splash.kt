6aae574f80d632adf881db6ef10d1f7f7bbf21f9
ibtihala817 <Ibtihal.ibrahim.dmm.k1@tuwaiq.edu.sa>
2021-12-10 18:22:33 +0300

    ending splash

--- app/src/main/java/com/example/mrshopercapstone/Splash.kt
+++ app/src/main/java/com/example/mrshopercapstone/Splash.kt
@@ -1,8 +1,11 @@
 package com.example.mrshopercapstone
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.example.mrshopercapstone.databinding.ActivitySplash2Binding
 
@@ -11,6 +14,21 @@ class Splash : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplash2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
+        ////////////////////////////////////////////////////////////
+        // for the top bar to gone in the splash and set the fullscreen in android R
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        //////////////////////////////////////////////////////////////
+       // this is for hiding the Action bar
+       supportActionBar?.hide()
+       //////////////////////////////////////////////
         binding = ActivitySplash2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         super.onCreate(savedInstanceState)
